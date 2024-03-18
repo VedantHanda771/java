5fc18ea34b5eb3ee5c91005d581a81b3a0aefdc4
VedantHanda771 <vedanthanda123@gmail.com>
2024-03-18 15:29:25 +0530

    loops

---
+++ loops.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class loops {
+    public static void main(String[] args){
+        // for (int i = 0;i<11;i++){
+        //     System.out.println(i);
+        // }
+        
+        // int i=0;
+        // while(i<11){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // int i=0;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // } while(i<11);
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        // int sum=0;
+        // for (int i=1;i<=n;i++){
+        //     sum=sum+i;
+        // }
+        // System.out.println(sum);
+
+        for (int i=1;i<=10;i++){
+            System.out.println(n*i);
+            
+        } 
+
+    }   
+}
