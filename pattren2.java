0a80f0c97bf7a36a0664258066ec3eb5f25752ae
VedantHanda771 <vedanthanda123@gmail.com>
2024-03-21 15:25:27 +0530

    pattren

--- pattren2.java
+++ pattren2.java
@@ -37,21 +37,76 @@ public class pattren2 {
         // }
 
         // sharme vala
-        for (int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print(j);
+        // for (int i=n;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+
+        //     int star=2*(n-i);
+        //     for(int j=1;j<=star;j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j=i;j>=1;j--){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // pyramid
+        // for (int i=1;i<=n;i++){
+        //     for (int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for (int j=1;j<=i;j++){
+        //         System.out.print(i+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // palindorme pyarmid
+
+        // for (int i=1;i<=n;i++){
+        //     for (int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+
+        //     for (int j=i;j>=1;j--){
+        //         System.out.print(j);
+        //     }
+
+        //     for (int j=2;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
             }
 
-            int star=2*(n-i);
-            for(int j=1;j<=star;j++){
-                System.out.print("*");
+            for (int j=1;j<=i;j++){
+                System.out.print("*" +" ");
             }
-            for(int j=i;j>=1;j--){
-                System.out.print(j);
-            }
+
             System.out.println();
         }
+        
+        for (int i=n;i>=1;i--){
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
 
+            for (int j=1;j<=i;j++){
+                System.out.print("*" +" ");
+            }
+
+            System.out.println();
+        }
+    
     }
+
+
+
     
 }
