33ba04c6f1d16525465b84104ae1ed17e8ce7db8
VedantHanda771 <vedanthanda123@gmail.com>
2024-03-27 00:22:17 +0530

    Commented out average method and added a new sum method that calculates the sum of odd numbers up to a given integer in the main method.

--- exercise1.java
+++ exercise1.java
@@ -3,18 +3,33 @@ import java.util.*;
 public class exercise1 {
 
 
-    public static int average(int a, int b, int c){
-        int avg= (a+b+c)/3;
-        return avg;
+    // public static int average(int a, int b, int c){
+    //     int avg= (a+b+c)/3;
+    //     return avg;
+
+    // }
+
+    public static int sum(int n){
+        int odd =0;
+        for(int i=1;i<=n;i+=2){
+            odd+=i;    
+        }
+        return odd;
+        
 
     }
     public static void main(String [] agrs){
         Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        // int c=sc.nextInt();
 
-         System.out.println(average(a,b,c));
+        //  System.out.println(average(a,b,c));
+        int n=sc.nextInt();
+        System.out.println(sum(n));
+    
     }
+
+
     
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+or (int j=2;j<=i;j++){
+            //     System.out.print(j);
+            // }
