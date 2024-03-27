de865984bd2f16b7b071cce85daf100b3fb55d06
VedantHanda771 <vedanthanda123@gmail.com>
2024-03-27 11:02:08 +0530

    exercise

--- exercise1.java
+++ exercise1.java
@@ -9,24 +9,61 @@ public class exercise1 {
 
     // }
 
-    public static int sum(int n){
-        int odd =0;
-        for(int i=1;i<=n;i+=2){
-            odd+=i;    
-        }
-        return odd;
+    // public static int sum(int n){
+    //     int odd =0;
+    //     for(int i=1;i<=n;i+=2){
+    //         odd+=i;    
+    //     }
+    //     return odd;
         
 
-    }
+    // }
+
+    // public static int greater(int a,int b){
+    //     if (a>b){
+    //         return a;
+
+    //     } else{
+    //         return b;
+    //     }
+        
+    // }
+
+    // public static double circumferance(int r){
+    //     double circum=(2*3.14)* r;
+    //     return circum;
+    // }
+
+    // public static void infinite(String s){
+    //     do{
+    //         System.out.println(s);
+    //     } while(true);
+    // }
+
+    
+
+
     public static void main(String [] agrs){
         Scanner sc=new Scanner(System.in);
         // int a=sc.nextInt();
         // int b=sc.nextInt();
         // int c=sc.nextInt();
 
+        
+
+        // int r=sc.nextInt();
+
+        // String s=sc.nextLine();
+
         //  System.out.println(average(a,b,c));
-        int n=sc.nextInt();
-        System.out.println(sum(n));
+        // int n=sc.nextInt();
+        // System.out.println(sum(n));
+        
+        // System.out.print(greater(a,b) +" Is Greater");
+
+        // System.out.println(circumferance(r));
+
+        // infinite(s);
     
     }
 
