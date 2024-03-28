ef3c2df874f6585c5847e6f7f0abaab715b83994
VedantHanda771 <vedanthanda123@gmail.com>
2024-03-28 09:26:09 +0530

    sec

--- zeros.java
+++ zeros.java
@@ -4,27 +4,37 @@ import java.util.*;
 public class zeros {
    public static void main(String args[]) {
        int positive = 0, negative = 0, zeros = 0;
-       System.out.println("Press 1 to continue & 0 to stop");
-       Scanner sc = new Scanner(System.in);
-       int input = sc.nextInt();
+    //    System.out.println("Press 1 to continue & 0 to stop");
+    //    Scanner sc = new Scanner(System.in);
+    //    int input = sc.nextInt();
 
 
-       while(input == 1) {
-           System.out.println("Enter your number : ");
-           int number = sc.nextInt();
-           if(number > 0) {
-               positive++;
-           } else if(number < 0) {
-               negative++;
-           } else {
-               zeros++;
-           }
+    //    while(input == 1) {
+    //        System.out.println("Enter your number : ");
+    //        int number = sc.nextInt();
+    //        if(number > 0) {
+    //            positive++;
+    //        } else if(number < 0) {
+    //            negative++;
+    //        } else {
+    //            zeros++;
+    //        }
 
 
-           System.out.println("Press 1 to continue & 0 to stop");
-           input = sc.nextInt();
-       }
+    //        System.out.println("Press 1 to continue & 0 to stop");
+    //        input = sc.nextInt();
+    //    }
 
+    // using while loop
+
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        while(n-->0){
+            int t=sc.nextInt();
+            if(t>0)positive++;
+            else if(t<0)negative++;
+            else zeros++;
+        }
 
        System.out.println("Positives : "+ positive);
        System.out.println("Negatives : "+ negative);
