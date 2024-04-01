98e5d98282118523ca1bf81fd50b4e0935cfd6a3
VedantHanda771 <vedanthanda123@gmail.com>
2024-04-01 11:45:41 +0530

    str

---
+++ strings.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+public class strings {
+
+    public static void main(String[] agrs){
+        Scanner sc=new Scanner(System.in);
+        // String name=sc.next();
+        // String fname= sc.nextLine();
+
+        // String full= name+ " "+fname;
+
+        // System.out.println(full.length());  //printing the
+
+
+        // // charAt
+
+        // for (int i=0;i<full.length();i++){
+        //     System.out.println(full.charAt(i));
+            
+        // }
+
+
+        String n1="vedant";
+        // String n2="handa";
+
+        // if(n1.compareTo(n2)>0){
+        //     System.out.println("n1 is greater");
+        // } else if(n1.compareTo(n2)<0){
+        //     System.out.println("n2 is greater");
+        // } else{
+        //     System.out.println("Both are equal");
+        // }
+
+        // substring
+        String name=n1.substring(2,5);
+        System.out.println(name);
+    }
+    
+}
