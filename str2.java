305f4ff784b0660aa94381296be3168fd0a3d1df
VedantHanda771 <vedanthanda123@gmail.com>
2024-04-02 09:43:07 +0530

    str2

--- str2.java
+++ str2.java
@@ -5,17 +5,46 @@ public class str2 {
 
     public static void main(String[] agrs){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter number of Strings:");
-        int n=sc.nextInt();
-        int tLength=0;
-        String arr[]=new String[n];
-        sc.nextLine();
-        for (int i=0;i<n;i++){
-            arr[i]=sc.nextLine();
-            tLength+=arr[i].length();
-        }
+        // System.out.println("enter number of Strings:");
+        // int n=sc.nextInt();
+        // int tLength=0;
+        // String arr[]=new String[n];
+        // sc.nextLine();
+        // for (int i=0;i<n;i++){
+        //     arr[i]=sc.nextLine();
+        //     tLength+=arr[i].length();
+        // }
 
-        System.out.println(tLength);
+        // System.out.println(tLength);
+
+        // String org=sc.next();
+        // String result="";
+
+        // for (int i=0;i<org.length();i++){
+        //     if(org.charAt(i)== 'e'){
+        //         result+='i';
+
+        //     } else{
+        //         result+=org.charAt(i);
+        //     }
+
+            
+        // }
+
+        // System.out.println(result);
+
+        String gmail=sc.next();
+        String userName = "";
+        for (int i=0;i<gmail.length();i++){
+            if(gmail.charAt(i)=='@'){
+                break;
+
+            } else {
+                userName+=gmail.charAt(i);
+            }
+
+        }
+        System.out.println(userName);
 
 
     }
