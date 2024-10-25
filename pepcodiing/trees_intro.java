c66692d6b06ba3c2a3d6409a8cc05a6827f3b072
VedantHanda771 <vedanthanda123@gmail.com>
2024-10-25 17:50:08 +0530

    trees

---
+++ pepcodiing/trees_intro.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class trees_intro {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data , Node left, Node right){
+            
+        }
+    } 
+}
